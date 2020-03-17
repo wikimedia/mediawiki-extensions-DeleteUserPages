f517a4920b3fab98759f63aba46b9d1ab8d789b5
DannyS712 <DannyS712.enwiki@gmail.com>
2020-03-17 20:12:45 +0000

    Remove use of Title::userCan in MW 1.33+
    Bug: T246139
    Change-Id: I36ea57d0af5028c3eb7eea7b7da7ac78e5f24f3f

--- includes/DeleteUserPages.php
+++ includes/DeleteUserPages.php
@@ -9,6 +9,8 @@
  * @link https://www.mediawiki.org/wiki/Extension:DeleteUserPages Documentation
  */
 
+use MediaWiki\MediaWikiServices;
+
 class DeleteUserPages {
 	public static function onTitleQuickPermissions( $title, $user, $action, &$errors, $doExpensiveQueries, $short ) {
 		if ( $action !== 'delete' || count( $errors ) > 0 ) {
@@ -20,10 +22,19 @@ class DeleteUserPages {
 		$root = $title->getRootText();
 		$text = $title->getText();
 
+		if ( class_exists( 'MediaWiki\Permissions\PermissionManager' ) ) {
+			// MW 1.33+
+			$userCan = MediaWikiServices::getInstance()
+				->getPermissionManager()
+				->userCan( 'edit', $user, $title );
+		} else {
+			$userCan = $title->userCan( 'edit' );
+		}
+
 		if (
 			( $ns === NS_USER || $ns === NS_USER_TALK )
 			&& $userName === $root
-			&& $title->userCan( 'edit' )
+			&& $userCan
 		) {
 			if ( $root === $text && $user->isAllowed( 'delete-rootuserpages' ) ) {
 				return false;
